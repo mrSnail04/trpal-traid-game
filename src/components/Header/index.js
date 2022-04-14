bbca82927bed943e3044f137e2d80b9bcc382aee
Maksim Bulauski <maks.bulavsky@gmail.com>
2022-04-14 19:59:41 +0300

    Удалил свои стили, оставил только из урока. Смог сделать меню по центру.Непонимаю как разнести лого и пункты меню по разным сторонам не исправляя css

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,7 @@
 import React from "react";
-import s from './Header.module.css';
 import Container from "../Container";
+import s from './Header.module.css';
+
 import logoPng from "../../assets/logo.png";
 
 const MENU = ['Menu 1', 'Menu 2', 'Menu 3', 'Menu 4'];
@@ -8,16 +9,16 @@ const MENU = ['Menu 1', 'Menu 2', 'Menu 3', 'Menu 4'];
 const Header = () => {
     return (
         <header className={s.root}>
-            <div className={s.header}>
-                <Container>
+            <Container>
+                <div className={s.header}>
                     <div className={s.logo}>
                         <img src={logoPng} alt="Logo"/>
                     </div>
                     <ul className={s.nav}>
-                        {MENU.map(itemMenu => <li><a href="#">{itemMenu}</a></li>)}
+                        {MENU.map(itemMenu => <li key={itemMenu}><a href="#">{itemMenu}</a></li>)}
                     </ul>
-                </Container>
-            </div>
+                </div>
+            </Container>
         </header>
     )
 };
