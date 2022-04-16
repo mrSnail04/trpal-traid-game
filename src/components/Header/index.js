691227fe7fc6798febced42020651327dceb81f3
Maksim Bulauski <maks.bulavsky@gmail.com>
2022-04-16 09:45:00 +0300

    upd

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,16 +9,16 @@ const MENU = ['Menu 1', 'Menu 2', 'Menu 3', 'Menu 4'];
 const Header = () => {
     return (
         <header className={s.root}>
-            <Container>
-                <div className={s.header}>
+            <div className={s.header}>
+                <Container className={s.headerWrap}>
                     <div className={s.logo}>
                         <img src={logoPng} alt="Logo"/>
                     </div>
                     <ul className={s.nav}>
                         {MENU.map(itemMenu => <li key={itemMenu}><a href="#">{itemMenu}</a></li>)}
                     </ul>
-                </div>
-            </Container>
+                </Container>
+            </div>
         </header>
     )
 };
